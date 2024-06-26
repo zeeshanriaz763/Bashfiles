45fd95c5a2ee702c184d93cd9c1e03845896fe0d
root <root@DESKTOP-Q9UBESA.>
2024-06-26 18:37:15 +0500

    Add new file zeeshan.sh

--- zeeshan.sh
+++ zeeshan.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
+echo "My name is Zeeshan Riaz"
+# Display system name
+echo "System Name: $(hostname)"
 
-# Function to get system name
-get_system_name() {
-    system_name=$(hostnamectl --static)
-    echo "System Name: $system_name"
-}
+# Number of running CPUs
+echo "CPU: $(nproc --all)"
 
-# Function to get CPU information
-get_cpu_info() {
-    cpu_info=$(lscpu | grep "Model name" | awk -F': ' '{print "CPU: " $2}')
-    echo "$cpu_info"
-}
+# RAM information
+echo "RAM: $(free -h | awk '/Mem:/ {print $2}')"
 
-# Function to get total RAM
-get_ram_info() {
-    ram_info=$(free -h | awk '/^Mem:/ {print "RAM: " $2}')
-    echo "$ram_info"
-}
+# Total SSD storage
+echo "Total SSD Storage: $(df -h | grep '/dev/sd' | awk '{sum += $2} END {print sum}')"
 
-# Function to get total SSD storage
-get_total_ssd_storage() {
-    total_ssd_storage=$(lsblk | grep "ssd" | awk '{sum += $4} END {print "Total SSD Storage: " sum}')
-    echo "$total_ssd_storage"
-}
-
-# Main function to display system information
-main() {
-    echo "System Information:"
-    get_system_name
-    get_cpu_info
-    get_ram_info
-    get_total_ssd_storage
-}
-
-# Call the main function
-main
